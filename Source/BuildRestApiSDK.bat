d8cedf1241af43dad28b7c0549f65192d82d3b2e
Juwon Lee <juwlee@paypal.com>
2014-04-29 16:04:18 -0500

    updated build script to create DLLs for unit tests

--- Source/BuildRestApiSDK.bat
+++ Source/BuildRestApiSDK.bat
@@ -1,6 +1,9 @@
 @echo off
+IF exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\" ( call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" RestApiSDK.VS.2008.sln /build Debug)
 IF exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\" ( call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" RestApiSDK.VS.2008.sln /build Release)
 
+IF exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\" ( call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" RestApiSDK.VS.2010.sln /build Debug)
 IF exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\" ( call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" RestApiSDK.VS.2010.sln /build Release)
 
+IF exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\" ( call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.com" RestApiSDK.VS.2012.sln /build Debug)
 IF exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\" ( call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.com" RestApiSDK.VS.2012.sln /build Release)
