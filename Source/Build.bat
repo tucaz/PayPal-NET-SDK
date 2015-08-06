f5b6db9ecce098c980822142a64c8df214c87f98
jziaja <jziaja@paypal.com>
2015-08-06 15:29:17 -0500

    Update Build.bat

--- Source/Build.bat
+++ Source/Build.bat
@@ -3,23 +3,25 @@
 ECHO PayPal .NET SDK
 ECHO ======================================
 
-IF "%VS120COMNTOOLS%"=="" GOTO :VS_NOT_FOUND
+SET VSTOOLS=%VS140COMNTOOLS%
+IF "%VSTOOLS%"=="" GOTO :VS_NOT_FOUND
 
-:: .NET 4.0
-"%VS120COMNTOOLS%\..\IDE\devenv.com" PayPal.SDK.NET40.sln /build Debug
-"%VS120COMNTOOLS%\..\IDE\devenv.com" PayPal.SDK.NET40.sln /build Release
+SET IDE_DIR=%VSTOOLS%\..\IDE
+SET DEVENV="%IDE_DIR%\devenv.com"
+%DEVENV% PayPal.SDK.NET40.sln /build Release
+%DEVENV% PayPal.SDK.NET45.sln /build Release
+%DEVENV% PayPal.SDK.NET451.sln /build Release
 
-:: .NET 4.5
-"%VS120COMNTOOLS%\..\IDE\devenv.com" PayPal.SDK.NET45.sln /build Debug
-"%VS120COMNTOOLS%\..\IDE\devenv.com" PayPal.SDK.NET45.sln /build Release
+DEL TestResults\results_net*.xml
 
-:: .NET 4.5.1
-"%VS120COMNTOOLS%\..\IDE\devenv.com" PayPal.SDK.NET451.sln /build Debug
-"%VS120COMNTOOLS%\..\IDE\devenv.com" PayPal.SDK.NET451.sln /build Release
-
+SET MSTEST="%IDE_DIR%\MSTest.exe"
+SET TEST_DLL=PayPal.Tests.dll
+%MSTEST% /testcontainer:Bin\Release\net40\%TEST_DLL% /category:Unit /resultsfile:TestResults\results_net40.xml
+%MSTEST% /testcontainer:Bin\Release\net45\%TEST_DLL% /category:Unit /resultsfile:TestResults\results_net45.xml
+%MSTEST% /testcontainer:Bin\Release\net451\%TEST_DLL% /category:Unit /resultsfile:TestResults\results_net451.xml
 GOTO :END
 
 :VS_NOT_FOUND
-ECHO Visual Studio 2013 was not found.
+ECHO Visual Studio 2015 was not found.
 
 :END
